8d0a5969362927fd1aca04bba3a6b5a5b7201078
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-08-09 11:19:17 -0700

    Added pre/post headers

--- src/HashTable.java
+++ src/HashTable.java
@@ -27,6 +27,13 @@ public class HashTable {
         this.currentSize = 0;
     }
 
+    /*
+    *Pre: key value of type Dollar
+    *
+    * Post: gets the whole and fractional value of key to get the total of the Hash Scheme
+    *
+    * Return: Integer result
+     */
     private int hashScheme(Dollar key) {
         int m = 2;
         int n = 3;
@@ -35,6 +42,13 @@ public class HashTable {
         return (m * whole + n * fraction) % totalBuckets;
     }
 
+    /*
+    *Pre: Key value of type dollar
+    *
+    *Post: Checks key value first if it exists in table, otherwise it finds its spot using hash scheme
+    *
+    * Return: None, type void
+     */
     public void hashInsert(Dollar key) {
         if (getIndex(key) != -1) {
             System.out.println("Dollar amount $" + key+ " Already in Hash Table.");
@@ -58,6 +72,13 @@ public class HashTable {
         collisions += count;
     }
 
+    /*
+    *Pre: dollar of type Dollar
+    *
+    *Post: Gets the index of a given dollar value, using hash scheme
+    *
+    * Return: Integer value
+     */
     public int getIndex(Dollar dollar) {
         int index = hashScheme(dollar);
         int ogIndex = index;
@@ -77,6 +98,13 @@ public class HashTable {
         return -1;
     }
 
+    /*
+    *Pre: NO parameters
+    *
+    *Post: Constructs a string by iterating through every index
+    *
+    * Return: None, type void.
+     */
     public void printHashTable() {
         for (int i = 0; i < totalBuckets; i++) {
             if (hashTable[i] != null) {
@@ -84,7 +112,7 @@ public class HashTable {
             }
         }
     }
-    
+     //Getters
     public int getCurrentSize() {
     	return currentSize;
     }
